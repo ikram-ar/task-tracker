f99fb2d0b4710e24d637a1c980cac115e948c80c
Ikram <arifikram2002@gmail.com>
2024-07-31 01:27:33 +0100

    Create Tasks and Task components and add a task list and delete functionality in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-// import React from 'react'; for class
-import Header from './components/Header'
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks ] = useState([
+    {
+        id: 1,
+        text: 'Complete React tutorial',
+        day: '2024-07-30',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Submit project proposal',
+        day: '2024-08-01',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Attend team meeting',
+        day: '2024-08-03',
+        reminder: true,
+    }, 
+])
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id ))
+}
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks to Show'}
     </div>
   );
 }
 
 
-// class App extends React.Component{
-//   render(){
-//     return <h1>Hello from a class</h1>
-//   }
-// }
+
 export default App;
